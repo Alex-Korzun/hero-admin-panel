9d1498b0bed41c06422393ba69b4fdbcba217726
Alex Korzun <koleksandr4@gmail.com>
2024-04-03 18:30:12 +0300

    init commit

---
+++ src/components/heroesListItem/HeroesListItem.js
@@ -0,0 +1,42 @@
+
+const HeroesListItem = ({name, description, element}) => {
+
+    let elementClassName;
+
+    switch (element) {
+        case 'fire':
+            elementClassName = 'bg-danger bg-gradient';
+            break;
+        case 'water':
+            elementClassName = 'bg-primary bg-gradient';
+            break;
+        case 'wind':
+            elementClassName = 'bg-success bg-gradient';
+            break;
+        case 'earth':
+            elementClassName = 'bg-secondary bg-gradient';
+            break;
+        default:
+            elementClassName = 'bg-warning bg-gradient';
+    }
+
+    return (
+        <li 
+            className={`card flex-row mb-4 shadow-lg text-white ${elementClassName}`}>
+            <img src="https://t3.ftcdn.net/jpg/02/98/30/88/360_F_298308844_EDQ9bpuDkPVhBT3ooRJdzjm2gQ3XPxOT.jpg" 
+                 className="img-fluid w-25 d-inline" 
+                 alt="unknown hero" 
+                 style={{'objectFit': 'cover'}}/>
+            <div className="card-body">
+                
+                <h3 className="card-title">{name}</h3>
+                <p className="card-text">{description}</p>
+            </div>
+            <span className="position-absolute top-0 start-100 translate-middle badge border rounded-pill bg-light">
+                <button type="button" className="btn-close btn-close" aria-label="Close"></button>
+            </span>
+        </li>
+    )
+}
+
+export default HeroesListItem;
